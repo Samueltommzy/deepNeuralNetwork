ed12355f8297a46a39ac8f2cde47e629e1891d56
Samueloluwatomi <samuel@grit.systems>
2018-09-18 10:43:38 +0100

    update on model

--- earthquake/earthquakePrediction.py
+++ earthquake/earthquakePrediction.py
@@ -6,16 +6,15 @@ from tensorflow import keras
 
 
 
-quakeDataset = pd.read_csv('./Datasets/database.csv')
+quakeDataset = pd.read_csv('../../Datasets/database.csv')
 quakeDataset = quakeDataset.reindex(np.random.permutation(quakeDataset.index))
 # print(quakeDataset.head())
 # print(" {} {}" .format( "info before parsing date" , quakeDataset.info()))
 quakeDataset['parsed_date'] = pd.to_datetime(quakeDataset['Date'] , format="%m/%d/%Y" , infer_datetime_format= True)
-input_features = quakeDataset[["parsed_date" , "Time" , "Latitude", "Longitude" ]]
+input_features = quakeDataset[[ "Latitude", "Longitude" ]]
 target_feature = quakeDataset[["Magnitude"]]
 test_input  = input_features.head(12000)
 test_target = target_feature.head(12000)
-
 validation_input = input_features.tail(11412)
 validation_target =target_feature.tail(11412)
 
@@ -27,11 +26,10 @@ print("description of validation target" , validation_target.describe(), "\n")
 model = keras.Sequential()
 model.add(keras.layers.Dense(64, activation= 'relu'))
 model.add(keras.layers.Dense(64, activation= 'relu'))
-model.add(keras.layers.Dense(10, activation='softmax'))
-
+model.add(keras.layers.Dense(units = 1, input_dim = 10, activation='softmax'))
 model.compile(optimizer = tf.train.AdamOptimizer() , loss='mse' , metrics=['mae'])
-model.fit(test_input,test_target,epochs = 10,batch_size = 50,validation_data = (validation_input,validation_target))
-print(model.summary())
+model.fit(test_input.values,test_target.values,epochs = 10,batch_size = 50,validation_data = (validation_input.values,validation_target.values))
+
 
 
 
